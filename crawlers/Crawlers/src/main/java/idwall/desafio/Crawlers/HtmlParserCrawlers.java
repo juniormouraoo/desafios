137c3e8e9cef210b083f12967bbc817f93ef84c6
Junior Mourão <junior.mourao.r@gmail.com>
2019-02-08 00:56:31 -0200

    Entrega Final Desafio Crawlers Parte 1

--- crawlers/Crawlers/src/main/java/idwall/desafio/Crawlers/HtmlParserCrawlers.java
+++ crawlers/Crawlers/src/main/java/idwall/desafio/Crawlers/HtmlParserCrawlers.java
@@ -16,11 +16,14 @@ public class HtmlParserCrawlers {
 	}
 
 	public void conexion(String pesquisa) {
+
+		// Separa as palavras passadas por parametro
 		String[] p = pesquisa.split(";");
 		String url = "https://old.reddit.com/r/";
 		for (String palavra : p) {
 
 			try {
+				// Conexao e captura das informacoes no old.reddit
 				Document document = Jsoup.connect(url + palavra + "/top/").get();
 				HtmlParserCrawlers parserCrawlers = new HtmlParserCrawlers(document);
 				parserCrawlers.getCrawlers(palavra);
@@ -28,22 +31,25 @@ public class HtmlParserCrawlers {
 				e.printStackTrace();
 			}
 		}
-
 	}
 
 	private void getCrawlers(String palavra) {
 
 		Boolean vote = false;
+		// Captura dos elementos
 		Elements elements = document.getElementsByClass("thing");
 		for (Element element : elements) {
+			// Captura de upvotes
 			String upvote = element.getElementsByClass("score unvoted").attr("title");
 			int upvote_int = Integer.parseInt(upvote);
 			if (upvote_int >= 5000) {
+				// Condicao para os dois nomes de titulos encontrados
 				String title = element.getElementsByClass("title may-blank ").text();
 				if (title == null || title.trim().isEmpty()) {
 					title = element.getElementsByClass("title may-blank outbound").text();
 				}
 				String href = element.getElementsByClass("bylink comments may-blank").attr("href");
+
 				System.out.println("Subreddit: " + palavra + "\nTítulo: " + title + "\nUpvotes: " + upvote
 						+ "\nLink para os " + "Comentarios: " + href
 						+ "\n-----------------------------------------------------------------------"
@@ -51,11 +57,12 @@ public class HtmlParserCrawlers {
 				vote = true;
 			}
 		}
+		// Retorno caso a pesquisa de uma das palavras não retorne algo que se encaixa
+		// nas condicoes
 		if (vote == false) {
 			System.out.println("A pesquisa " + palavra + " não possui nenhuma thread com mais de 5000 upvotes"
 					+ "\n-----------------------------------------------------------------------"
 					+ "--------------------------------------------------------------------");
 		}
 	}
-
 }
--- crawlers/Crawlers/src/main/java/idwall/desafio/Crawlers/Main.java
+++ crawlers/Crawlers/src/main/java/idwall/desafio/Crawlers/Main.java
@@ -1,11 +1,12 @@
 package idwall.desafio.Crawlers;
 
 public class Main {
-	
+
 	String pesquisa;
 
 	public static void main(String[] args) {
 
+		// Input do que sera pesquisado
 		String pesquisa = "worldnews;askreddit;cats;dogs";
 		HtmlParserCrawlers htmlParserCrawlers = new HtmlParserCrawlers(null);
 		htmlParserCrawlers.conexion(pesquisa);
